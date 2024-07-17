f83ac30c1be394e82a41573e01dfcca33b8635cf
r2hu1 <newasram86@gmail.com>
2024-07-17 23:01:48 +0530

    gridded theme switcher

--- components/modetoggle.jsx
+++ components/modetoggle.jsx
@@ -1,40 +1,23 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Monitor, Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { Button } from "./ui/button"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
-
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex gap-1 justify-center items-center">
+      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("dark")}>
+        <Moon className="h-4 w-4" />
+      </Button>
+      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("light")}>
+        <Sun className="h-4 w-4" />
+      </Button>
+      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("system")}>
+        <Monitor className="h-4 w-4" />
+      </Button>
+    </div>
   )
 }
