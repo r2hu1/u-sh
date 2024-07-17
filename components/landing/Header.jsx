7d048d785a84170643f3a154466af7539e5b8e19
r2hu1 <newasram86@gmail.com>
2024-07-17 23:03:43 +0530

    just to force vercel to deploy

--- components/landing/Header.jsx
+++ components/landing/Header.jsx
@@ -7,6 +7,7 @@ export default function Header(){
         <header className="flex items-center justify-between px-6 py-5 md:px-20 lg:px-32">
         <div>
           <Link2 className="h-6 w-6" />
+           
         </div>
         <div className="flex items-center gap-3">
           <Link className={buttonVariants({ variant: "outline" })} href="/">Login</Link>
