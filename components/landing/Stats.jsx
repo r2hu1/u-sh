66212631909032d6a328e42cac02d4918a558133
Rahul Rajput <newasram86@gmail.com>
2024-07-18 20:50:35 +0530

    Update Stats.jsx

--- components/landing/Stats.jsx
+++ components/landing/Stats.jsx
@@ -6,21 +6,21 @@ export default function Stats() {
       <div className="flex flex-wrap gap-5 items-center px-4 md:px-5 md:py-5 justify-center border border-border py-4 rounded-lg">
         <div className="grid !gap-0">
           <div className='flex items-center justify-center'>
-            <CountUp start={000} end={247} className='text-2xl md:text-2.5xl' />
+            <CountUp start={001} end={247} className='text-2xl md:text-2.5xl' />
             <span className='text-2xl md:text-2.5xl'>+</span>
           </div>
           <p className="text-muted-foreground text-sm sm:text-base">registered users</p>
         </div>
         <div className="grid !gap-0">
         <div className='flex items-center justify-center'>
-            <CountUp start={0000} end={1849} className='text-2xl md:text-2.5xl' />
+            <CountUp start={0001} end={1849} className='text-2xl md:text-2.5xl' />
             <span className='text-2xl md:text-2.5xl'>+</span>
           </div>
           <p className="text-muted-foreground text-sm sm:text-base">links shorted</p>
         </div>
         <div className="grid !gap-0">
         <div className='flex items-center justify-center'>
-            <CountUp start={00000} end={85679} className='text-2xl md:text-2.5xl' />
+            <CountUp start={00001} end={85679} className='text-2xl md:text-2.5xl' />
             <span className='text-2xl md:text-2.5xl'>+</span>
           </div>
           <p className="text-muted-foreground text-sm sm:text-base">link clicks</p>
