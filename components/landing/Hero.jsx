38041d4330784f390f1ba587cffb8eb30afb6f66
r2hu1 <newasram86@gmail.com>
2024-07-18 19:53:25 +0530

    fnl

--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl">Open-Source url shortener built with <span className="underline underline-offset-[5px] decoration-blue-400">nextjs</span> & <span className="underline underline-offset-[5px] decoration-blue-400">mongodb</span></h1>
           <p className="text-muted-foreground sm:mt-2 text-sm sm:text-base max-w-2xl">I have coded this url shortner using nextjs 14, shadcn/ui & mongodb client mongoose with detailed analytics of links being clicked!</p>
           <div className="flex items-center justify-center flex-wrap gap-3 mt-3">
-            <Link href="/" className={cn(buttonVariants({ variant: "default" }), "flex items-center gap-1")}><Star className="h-3.5 w-3.5" />Star on Github</Link>
+            <Link href="https://github.com/r2hu1/url-shortner" className={cn(buttonVariants({ variant: "default" }), "flex items-center gap-1")}><Star className="h-3.5 w-3.5" />Star on Github</Link>
             <Link href="/" className={buttonVariants({ variant: "outline" })}>Get Started</Link>
           </div>
         </div>
