fa369b468befaf7602ea68a67a3d2b16623a5786
r2hu1 <newasram86@gmail.com>
2024-07-19 20:05:43 +0530

    scr route

--- middleware.js
+++ middleware.js
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+]);
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
