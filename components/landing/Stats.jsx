706944751dd8c90b4b01ae6bacb646cd0a803470
r2hu1 <newasram86@gmail.com>
2024-07-19 20:05:16 +0530

    sm shng

--- components/landing/Stats.jsx
+++ components/landing/Stats.jsx
@@ -1,7 +1,7 @@
 export default function Stats() {
   return (
     <section className="mb-14 mt-3 px-6 md:px-20 lg:px-32 text-center w-fit mx-auto">
-      <div className="flex flex-wrap gap-5 items-center px-4 md:px-5 md:py-5 justify-center border border-border py-4 rounded-lg">
+      <div className="flex flex-wrap gap-5 items-center justify-center">
         <div className="grid !gap-0">
           <div className='flex items-center justify-center'>
             <h3 className="text-2xl md:text-2.5xl">247</h3>
