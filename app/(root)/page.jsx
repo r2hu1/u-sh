1f380bc713cccab7a30b7224503693c65c790fbf
r2hu1 <newasram86@gmail.com>
2024-07-19 20:05:38 +0530

    l

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,15 +2,17 @@ import Hero from "@/components/landing/Hero";
 import Stats from "@/components/landing/Stats";
 import Feature from "@/components/landing/Feature";
 import Pricing from "@/components/landing/Pricing";
+import Review from "@/components/landing/Review";
 
 export default function Page() {
- 
+
   return (
     <main>
-      <Hero/>
-      <Stats/>
-      <Feature/>
-      <Pricing/>
+      <Hero />
+      {/* <Stats /> */}
+      <Review/>
+      <Feature />
+      <Pricing />
     </main>
   )
 }
