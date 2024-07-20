ec4e0e2df08971db49744d3deabf8d0f1289bf1a
r2hu1 <newasram86@gmail.com>
2024-07-20 12:18:57 +0530

    sm chng

--- components/modetoggle.jsx
+++ components/modetoggle.jsx
@@ -4,18 +4,19 @@ import * as React from "react"
 import { Monitor, Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "./ui/button"
+import { cn } from "@/lib/utils"
 
-export function ModeToggle() {
+export function ModeToggle({btnClassName}) {
   const { setTheme } = useTheme()
   return (
     <div className="flex gap-1 justify-center items-center">
-      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("dark")}>
+      <Button size="sm" className={cn("h-[26px] w-[26px] p-0",btnClassName)} variant={"outline"} onClick={() => setTheme("dark")}>
         <Moon className="h-4 w-4" />
       </Button>
-      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("light")}>
+      <Button size="sm" className={cn("h-[26px] w-[26px] p-0",btnClassName)} variant={"outline"} onClick={() => setTheme("light")}>
         <Sun className="h-4 w-4" />
       </Button>
-      <Button size="sm" className="h-[26px] w-[26px] p-0" variant={"outline"} onClick={() => setTheme("system")}>
+      <Button size="sm" className={cn("h-[26px] w-[26px] p-0",btnClassName)} variant={"outline"} onClick={() => setTheme("system")}>
         <Monitor className="h-4 w-4" />
       </Button>
     </div>
