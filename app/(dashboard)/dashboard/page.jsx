4f0543644d62e9102067c514e62c054669edceb0
r2hu1 <newasram86@gmail.com>
2024-07-20 12:56:01 +0530

    te, dash lay

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,7 +1,36 @@
+import MainChart from "@/components/dashboard/MainChart";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function page() {
     return (
-        <main>
+        <main className="px-6 md:px-20 lg:px-32 py-10 grid gap-7">
+            <form className="grid gap-3 px-4 py-4 border-border border rounded-lg">
+                <div className="mb-2">
+                    <h2 className="text-lg">Short Link</h2>
+                    <p className="text-sm text-muted-foreground">short your link in seconds.</p>
+                </div>
+                <Label htmlFor="url">Your URL</Label>
+                <Input placeholder="https://example.com/dfghj.." name="url" />
+                <Label htmlFor="alias">Alias <span className="text-xs text-muted-foreground">(optional)<span className="text-blue-400">*</span></span></Label>
+                <Input placeholder="somethi.." name="alias" />
+                <Button variant="shine" className="mt-3">Shorten</Button>
+            </form>
+
+            <div className="grid gap-4 px-4 py-4 border-border border rounded-lg">
+                <div>
+                    <h2 className="text-lg">Recent Links</h2>
+                    <p className="text-sm text-muted-foreground">list of yout recently shortened links.</p>
+                </div>
+                <div className="h-40 grid place-content-center border border-dashed border-border rounded-lg">
+                    <p className="text-center text-sm text-muted-foreground">No links yet</p>
+                </div>
+            </div>
+
+            <div>
+                <MainChart />
+            </div>
         </main>
     )
 }
---
+++ components/dashboard/MainChart.jsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+]
+
+const chartConfig = {
+  views: {
+    label: "views",
+    color: "hsl(var(--chart-1))",
+  },
+};
+
+export default function MainChart() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg font-normal">Total visitors</CardTitle>
+        <CardDescription className="!-mt-[1px]">
+          showing total visitors for the last 6 months
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Area
+              dataKey="views"
+              type="natural"
+              fill="var(--color-views)"
+              fillOpacity={0.4}
+              stroke="var(--color-views)"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 font-medium leading-none">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
