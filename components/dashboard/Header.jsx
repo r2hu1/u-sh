b80cc6704e38891338fb45a57ed945c9e1cb4181
r2hu1 <newasram86@gmail.com>
2024-07-20 12:18:51 +0530

    almt rdy

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -6,27 +6,66 @@ import {
     Sheet,
     SheetContent,
     SheetDescription,
+    SheetFooter,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet";
 import { Skeleton } from "../ui/skeleton";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "../modetoggle";
 
 export default function Header() {
+    const [pathName, setPathName] = useState("");
+    const defaultClass = "transition h-10 rounded-l-lg border-r-2 border-transparent hover:border-blue-400 text-center hover:bg-accent/40 grid place-content-center text-base";
+    const activeClass = "border-r-2 border-blue-400 bg-accent/40";
+    const getPageUrl = () => {
+        setPathName(window.location.pathname);
+        return true;
+    }
+    useEffect(() => {
+        getPageUrl();
+    }, []);
     return (
         <header className="px-6 md:px-20 lg:px-32 py-4 flex items-center justify-between">
             <Sheet>
                 <SheetTrigger asChild>
-                    <div className="flex items-center justify-center border border-border rounded-lg h-10 w-10 hover:bg-secondary cursor-pointer transition">
-                        <PanelTopOpen className="h-4 w-4 rotate-[90deg]" />
+                    <div className="flex items-center justify-center h-10 w-10 rounded-lg cursor-pointer transition">
+                        <PanelTopOpen className="h-6 w-6 rotate-[90deg]" />
                     </div>
                 </SheetTrigger>
-                <SheetContent>
+                <SheetContent side="left" className="flex flex-col justify-between sm:max-w-xs">
                     <SheetHeader>
                         <SheetDescription className="mt-10">
-                            in dev
+                            <div className="grid gap-2">
+                                <Link className={cn(defaultClass, pathName == "/dashboard" && activeClass)} href="/">Dashboard</Link>
+                                <Link className={cn(defaultClass, pathName == "/links" && activeClass)} href="/">All Links</Link>
+                                <Link className={cn(defaultClass, pathName == "/statistics" && activeClass)} href="/">Statistics</Link>
+                            </div>
                         </SheetDescription>
                     </SheetHeader>
+                    <SheetFooter>
+                        <SheetDescription className="mt-10 grid gap-7 w-full">
+                            {/* <div className="flex items-center justify-between">
+                                <Link href="https://github.com/r2hu1/url-shortner">Github Repo</Link>
+                                <Link href="mailto:hi@rahul.eu.org">Hire Me</Link>
+                                <Link href="https://rahul.eu.org">My Portfolio</Link>
+                            </div> */}
+                            <div className="flex items-center justify-between gap-3">
+                                <div className="grid !gap-0.5">
+                                    <div className="border rounded-lg overflow-hidden">
+                                        <ModeToggle btnClassName="border-0 rounded-none" />
+                                    </div>
+                                    <span className="text-xs text-opacity-75 text-center">Change Theme</span>
+                                </div>
+                                <SignOutButton redirectUrl="/">
+                                    <Button>Logout</Button>
+                                </SignOutButton>
+                            </div>
+                        </SheetDescription>
+                    </SheetFooter>
                 </SheetContent>
             </Sheet>
 
