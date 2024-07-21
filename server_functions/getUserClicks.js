6a011fdbdea7ab7d1cad62d82deb0637723847bc
r2hu1 <newasram86@gmail.com>
2024-07-21 19:32:57 +0530

    []

--- server_functions/getUserClicks.js
+++ server_functions/getUserClicks.js
@@ -8,7 +8,7 @@ export const getUserClicks = async () => {
     const user = await currentUser();
     await connectToDb();
     try {
-        const getLinks = await short_url.find({});
+        const getLinks = await short_url.find({ author: user.emailAddresses[0].emailAddress });
         let allClicks = 0;
         for (let i = 0; i < getLinks.length; i++) {
             allClicks += getLinks[i].clicks;
