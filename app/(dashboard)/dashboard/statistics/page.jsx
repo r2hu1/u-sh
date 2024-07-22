6df3ff4137e2e4369e423da620eb3a5a0bff2753
r2hu1 <newasram86@gmail.com>
2024-07-22 19:32:11 +0530

    res

--- app/(dashboard)/dashboard/statistics/page.jsx
+++ app/(dashboard)/dashboard/statistics/page.jsx
@@ -22,7 +22,7 @@ export default function page() {
         getAllData();
     }, []);
     return (
-        <main className="px-6 md:px-20 lg:px-32 py-10 grid gap-7">
+        <main className="px-6 md:px-20 lg:px-44 py-10 grid gap-7">
             <div className="border rounded-lg grid grid-cols-2 bg-card shadow-sm">
                 <div className="text-center p-4 border-r border-border">
                     <h2 className="text-2xl">{totalLinks.length}</h2>
@@ -34,60 +34,63 @@ export default function page() {
                 </div>
             </div>
 
-            <div className="border border-border rounded-lg px-4 py-4 bg-card shadow-sm">
-                <div>
-                    <h2 className="text-lg">Top 5 Links</h2>
-                    <p className="text-sm text-muted-foreground">the top five links by clicks</p>
-                </div>
-                <div className="border-dashed rounded-lg mt-4 border border-border">
-                    {loading && (
-                        <div className="grid place-items-center h-60">
-                            <Loader2 className="animate-spin h-4 w-5" />
-                        </div>
-                    )}
-                    {!loading && !top5Links.length ? (
-                        <div className="grid place-items-center h-60">
-                            <p className="text-sm text-muted-foreground">No links found.</p>
-                        </div>
-                    ) : null}
-                    {top5Links.filter((link) => link.clicks).map((link) => (
-                        <div className="grid gap-2 p-4 linkList">
-                            <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
-                                <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit">{link.clicks > 0 ? link.clicks - 1 : link.clicks} <Eye className="h-4 w-4" /></p>
-                            </Link>
-                        </div>
-                    ))}
-                    {!loading && !top5Links.filter((link) => link.clicks).length && 
-                        (<div className="grid gap-2 p-4 place-items-center h-60">
-                            <p className="text-sm text-muted-foreground">No links found.</p>
-                        </div>)
-                    }
-                </div>
-            </div>
+            <div className="grid gap-7 md:gap-3 md:grid-cols-2">
 
-            <div className="border border-border rounded-lg px-4 py-4 bg-card shadow-sm">
-                <div>
-                    <h2 className="text-lg">All Links & Clicks</h2>
-                    <p className="text-sm text-muted-foreground">all your links and their clicks</p>
+                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
+                    <div>
+                        <h2 className="text-lg">Top 5 Links</h2>
+                        <p className="text-sm text-muted-foreground">the top five links by clicks</p>
+                    </div>
+                    <div className="border-dashed rounded-lg mt-4 border border-border">
+                        {loading && (
+                            <div className="grid place-items-center h-60">
+                                <Loader2 className="animate-spin h-4 w-5" />
+                            </div>
+                        )}
+                        {!loading && !top5Links.length ? (
+                            <div className="grid place-items-center h-60">
+                                <p className="text-sm text-muted-foreground">No links found.</p>
+                            </div>
+                        ) : null}
+                        {top5Links.filter((link) => link.clicks).map((link) => (
+                            <div className="grid gap-2 p-4 linkList">
+                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
+                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit">{link.clicks > 0 ? link.clicks - 1 : link.clicks} <Eye className="h-4 w-4" /></p>
+                                </Link>
+                            </div>
+                        ))}
+                        {!loading && !top5Links.filter((link) => link.clicks).length &&
+                            (<div className="grid gap-2 p-4 place-items-center h-60">
+                                <p className="text-sm text-muted-foreground">No links found.</p>
+                            </div>)
+                        }
+                    </div>
                 </div>
-                <div className="border-dashed rounded-lg mt-4 border border-border">
-                    {loading && (
-                        <div className="grid place-items-center h-52">
-                            <Loader2 className="animate-spin h-4 w-5" />
-                        </div>
-                    )}
-                    {!loading && !totalLinks.length ? (
-                        <div className="grid place-items-center h-60">
-                            <p className="text-sm text-muted-foreground">No links found.</p>
-                        </div>
-                    ) : null}
-                    {totalLinks.map((link) => (
-                        <div className="grid gap-2 p-4 linkList">
-                            <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
-                                <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit">{link.clicks > 0 ? link.clicks - 1 : link.clicks} <Eye className="h-4 w-4" /></p>
-                            </Link>
-                        </div>
-                    ))}
+
+                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
+                    <div>
+                        <h2 className="text-lg">All Links & Clicks</h2>
+                        <p className="text-sm text-muted-foreground">all your links and their clicks</p>
+                    </div>
+                    <div className="border-dashed rounded-lg mt-4 border border-border">
+                        {loading && (
+                            <div className="grid place-items-center h-60">
+                                <Loader2 className="animate-spin h-4 w-5" />
+                            </div>
+                        )}
+                        {!loading && !totalLinks.length ? (
+                            <div className="grid place-items-center h-60">
+                                <p className="text-sm text-muted-foreground">No links found.</p>
+                            </div>
+                        ) : null}
+                        {totalLinks.map((link) => (
+                            <div className="grid gap-2 p-4 linkList">
+                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
+                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit">{link.clicks > 0 ? link.clicks - 1 : link.clicks} <Eye className="h-4 w-4" /></p>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </main>
