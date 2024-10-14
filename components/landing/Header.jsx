87594ed57a1c62ab13d5fb65608f64f871a4a849
r2hu1 <hi@rahul.eu.org>
2024-10-14 21:44:02 +0530

    dec py and btn size

--- components/landing/Header.jsx
+++ components/landing/Header.jsx
@@ -6,22 +6,23 @@ import { SignOutButton } from "@clerk/nextjs";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-6 py-5 md:px-20 lg:px-32">
-      <div>
-        <Link2 className="h-6 w-6" />
+    <header className="flex items-center border-b justify-between px-5 py-3 md:px-20 lg:px-32 mb-10">
+      <div className="flex items-center gap-2">
+        <Link2 className="h-5 w-5" />
+        <span className="font-medium">u-sh</span>
       </div>
       <div className="flex items-center gap-3">
         <IfLoggedInElse ifNot={
-          <Link className={buttonVariants({ variant: "outline" })} href="/sign-in">Login</Link>
+          <Link className={buttonVariants({ variant: "outline",size: "sm" })} href="/sign-in">Login</Link>
         } ifUser={
           <SignOutButton redirectUrl="/">
-            <Button variant="outline">Logout</Button>
+            <Button variant="outline" size="sm">Logout</Button>
           </SignOutButton>
         } />
         <IfLoggedInElse ifNot={
-          <Link className={buttonVariants({ variant: "shine" })} href="/sign-up">Get Started</Link>
+          <Link className={buttonVariants({ variant: "shine", size: "sm" })} href="/sign-up">Get Started</Link>
         } ifUser={
-          <Link className={buttonVariants({ variant: "shine" })} href="/dashboard">Dashboard</Link>
+          <Link className={buttonVariants({ variant: "shine", size: "sm" })} href="/dashboard">Dashboard</Link>
         } />
       </div>
     </header>
