55c9f3840cf7a1b92f65f46171c0886ca406a4ce
Agamya Samuel <github@agamya.dev>
2026-01-20 00:00:52 +0530

    feat: enhance link statistics display with top links and all links sections

--- app/(dashboard)/dashboard/statistics/page.jsx
+++ app/(dashboard)/dashboard/statistics/page.jsx
@@ -138,6 +138,61 @@ export default function page() {
                 </div>
             </div>
             
+            {/* Top Links and All Links */}
+            <div className="grid gap-7 md:gap-3 md:grid-cols-2">
+                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
+                    <div>
+                        <h2 className="text-lg">Top {top5Links.filter((link) => link.clicks).length} Links</h2>
+                        <p className="text-sm text-muted-foreground">the top {top5Links.filter((link) => link.clicks).length} links by clicks</p>
+                    </div>
+                    <div className="border-dashed rounded-lg mt-4 border border-border">
+                        {loading && (
+                            <div className="grid place-items-center h-60">
+                                <Loader2 className="animate-spin h-4 w-5" />
+                            </div>
+                        )}
+                        {top5Links.filter((link) => link.clicks).map((link) => (
+                            <div key={link._id} className="grid gap-2 p-4 linkList">
+                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
+                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit"><CountUp end={link.clicks > 2 ? link.clicks - 1 : link.clicks} start={0} /> <Eye className="h-4 w-4" /></p>
+                                </Link>
+                            </div>
+                        ))}
+                        {!loading && !top5Links.filter((link) => link.clicks).length &&
+                            (<div className="grid gap-2 p-4 place-items-center h-60">
+                                <p className="text-sm text-muted-foreground">No links found.</p>
+                            </div>)
+                        }
+                    </div>
+                </div>
+
+                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
+                    <div>
+                        <h2 className="text-lg">All Links & Clicks</h2>
+                        <p className="text-sm text-muted-foreground">all your links and their clicks</p>
+                    </div>
+                    <div className="border-dashed rounded-lg mt-4 border border-border">
+                        {loading && (
+                            <div className="grid place-items-center h-60">
+                                <Loader2 className="animate-spin h-4 w-5" />
+                            </div>
+                        )}
+                        {!loading && !totalLinks.length ? (
+                            <div className="grid place-items-center h-60">
+                                <p className="text-sm text-muted-foreground">No links found.</p>
+                            </div>
+                        ) : null}
+                        {totalLinks.map((link) => (
+                            <div key={link._id} className="grid gap-2 p-4 linkList">
+                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
+                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit"><CountUp end={link.clicks > 2 ? link.clicks - 1 : link.clicks} start={0} /> <Eye className="h-4 w-4" /></p>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            
             {/* Time Series Chart */}
             <Card>
                 <CardHeader>
@@ -449,66 +504,6 @@ export default function page() {
                         )}
                     </CardContent>
                 </Card>
-            </div>
-
-            <div className="grid gap-7 md:gap-3 md:grid-cols-2">
-
-                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
-                    <div>
-                        <h2 className="text-lg">Top {top5Links.filter((link) => link.clicks).length} Links</h2>
-                        <p className="text-sm text-muted-foreground">the top {top5Links.filter((link) => link.clicks).length} links by clicks</p>
-                    </div>
-                    <div className="border-dashed rounded-lg mt-4 border border-border">
-                        {loading && (
-                            <div className="grid place-items-center h-60">
-                                <Loader2 className="animate-spin h-4 w-5" />
-                            </div>
-                        )}
-                        {/* {!loading && !top5Links.length ? (
-                            <div className="grid place-items-center h-60">
-                                <p className="text-sm text-muted-foreground">No links found.</p>
-                            </div>
-                        ) : null} */}
-                        {top5Links.filter((link) => link.clicks).map((link) => (
-                            <div key={link._id} className="grid gap-2 p-4 linkList">
-                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
-                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit"><CountUp end={link.clicks > 2 ? link.clicks - 1 : link.clicks} start={0} /> <Eye className="h-4 w-4" /></p>
-                                </Link>
-                            </div>
-                        ))}
-                        {!loading && !top5Links.filter((link) => link.clicks).length &&
-                            (<div className="grid gap-2 p-4 place-items-center h-60">
-                                <p className="text-sm text-muted-foreground">No links found.</p>
-                            </div>)
-                        }
-                    </div>
-                </div>
-
-                <div className="border h-fit border-border rounded-lg px-4 py-4 bg-card shadow-sm">
-                    <div>
-                        <h2 className="text-lg">All Links & Clicks</h2>
-                        <p className="text-sm text-muted-foreground">all your links and their clicks</p>
-                    </div>
-                    <div className="border-dashed rounded-lg mt-4 border border-border">
-                        {loading && (
-                            <div className="grid place-items-center h-60">
-                                <Loader2 className="animate-spin h-4 w-5" />
-                            </div>
-                        )}
-                        {!loading && !totalLinks.length ? (
-                            <div className="grid place-items-center h-60">
-                                <p className="text-sm text-muted-foreground">No links found.</p>
-                            </div>
-                        ) : null}
-                        {totalLinks.map((link) => (
-                            <div key={link._id} className="grid gap-2 p-4 linkList">
-                                <Link target="_blank" className="text-sm opacity-85 flex items-center justify-between" href={`https://${location.host}/${link.alias}`}>
-                                    <span className="hover:underline">https://{location.host}/{link.alias}</span> <p className="text-sm text-muted-foreground flex items-center gap-1 bg-accent/50 rounded-full px-2 cursor-pointer py-1 w-fit"><CountUp end={link.clicks > 2 ? link.clicks - 1 : link.clicks} start={0} /> <Eye className="h-4 w-4" /></p>
-                                </Link>
-                            </div>
-                        ))}
-                    </div>
-                </div>
             </div>
         </main>
     )
