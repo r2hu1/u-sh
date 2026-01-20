af77fd737b9218e6ccdb7edce2e69009ab89b84a
r2hu1 <newasram86@gmail.com>
2026-01-20 13:15:12 +0530

    n p

--- components/landing/Header.jsx
+++ components/landing/Header.jsx
@@ -5,49 +5,49 @@ import IfLoggedInElse from "../helpers/ifLoggedInElse";
 import { SignOutButton } from "@clerk/nextjs";
 
 export default function Header() {
-	return (
-		<header className="flex items-center justify-between px-6 py-5 max-w-6xl mx-auto">
-			<div className="flex items-center gap-2">
-				<Link2 className="h-4 w-4" />
-				<span className="font-medium">u-sh</span>
-			</div>
-			<div className="flex items-center gap-3">
-				<IfLoggedInElse
-					ifNot={
-						<Link
-							className={buttonVariants({ variant: "outline", size: "sm" })}
-							href="/sign-in"
-						>
-							Login
-						</Link>
-					}
-					ifUser={
-						<SignOutButton redirectUrl="/">
-							<Button variant="outline" size="sm">
-								Logout
-							</Button>
-						</SignOutButton>
-					}
-				/>
-				<IfLoggedInElse
-					ifNot={
-						<Link
-							className={buttonVariants({ variant: "shine", size: "sm" })}
-							href="/sign-up"
-						>
-							Get Started
-						</Link>
-					}
-					ifUser={
-						<Link
-							className={buttonVariants({ variant: "shine", size: "sm" })}
-							href="/dashboard"
-						>
-							Dashboard
-						</Link>
-					}
-				/>
-			</div>
-		</header>
-	);
+  return (
+    <header className="flex items-center justify-between px-6 py-5 max-w-6xl mx-auto">
+      <div className="flex items-center gap-2 bg-secondary/80 px-2.5 py-0.5 border border-input rounded-md">
+        <Link2 className="h-4 w-4" />
+        <span className="text-lg">u-sh</span>
+      </div>
+      <div className="flex items-center gap-3">
+        <IfLoggedInElse
+          ifNot={
+            <Link
+              className={buttonVariants({ variant: "outline", size: "sm" })}
+              href="/sign-in"
+            >
+              Login
+            </Link>
+          }
+          ifUser={
+            <SignOutButton redirectUrl="/">
+              <Button variant="outline" size="sm">
+                Logout
+              </Button>
+            </SignOutButton>
+          }
+        />
+        <IfLoggedInElse
+          ifNot={
+            <Link
+              className={buttonVariants({ variant: "shine", size: "sm" })}
+              href="/sign-up"
+            >
+              Get Started
+            </Link>
+          }
+          ifUser={
+            <Link
+              className={buttonVariants({ variant: "shine", size: "sm" })}
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          }
+        />
+      </div>
+    </header>
+  );
 }
--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -5,57 +5,57 @@ import { Star } from "lucide-react";
 import IfLoggedInElse from "../helpers/ifLoggedInElse";
 
 export default function Hero() {
-	return (
-		<section className="py-20 sm:mt-5 px-6 max-w-6xl mx-auto">
-			<div className="grid gap-6 max-w-5xl mx-auto text-center place-items-center">
-				<h1 className="font-semibold text-4xl md:text-5xl !leading-tight lg:text-6xl">
-					Open-Source url shortener built with{" "}
-					<span className="underline underline-offset-[5px] decoration-blue-400">
-						nextjs
-					</span>{" "}
-					&{" "}
-					<span className="underline underline-offset-[5px] decoration-blue-400">
-						mongodb
-					</span>
-				</h1>
-				<p className="text-foreground/80 mt-2 md:text-xl text-base max-w-2xl">
-					I have coded this url shortner using nextjs 14, shadcn/ui & mongodb
-					client mongoose with detailed analytics of links being clicked!
-				</p>
-				<div className="flex items-center sm:justify-center flex-wrap gap-3 mt-3">
-					<Link
-						href="https://github.com/r2hu1/u-sh"
-						className={cn(
-							buttonVariants({ variant: "outline" }),
-							"flex items-center gap-1",
-						)}
-					>
-						Star on Github
-						<Star className="h-3.5 w-3.5" />
-					</Link>
-					<IfLoggedInElse
-						ifNot={
-							<Link
-								href="/sign-up"
-								className={buttonVariants({ variant: "default" })}
-							>
-								Get Started
-							</Link>
-						}
-						ifUser={
-							<Link
-								href="/dashboard"
-								className={buttonVariants({ variant: "default" })}
-							>
-								Dashboard
-							</Link>
-						}
-					/>
-				</div>
-			</div>
-			<div className="w-fit h-fit mt-20 sm:mt-32 border rounded-lg overflow-hidden shadow-lg">
-				<img src="/d-preview.png" alt="Hero Image" />
-			</div>
-		</section>
-	);
+  return (
+    <section className="py-20 sm:mt-5 px-6 max-w-6xl mx-auto">
+      <div className="grid gap-6 max-w-5xl mx-auto text-center place-items-center">
+        <h1 className="font-semibold text-4xl md:text-5xl !leading-tight lg:text-6xl">
+          Open-Source url shortener built with{" "}
+          <span className="underline underline-offset-[5px] decoration-blue-400">
+            nextjs
+          </span>{" "}
+          &{" "}
+          <span className="underline underline-offset-[5px] decoration-blue-400">
+            mongodb
+          </span>
+        </h1>
+        <p className="text-foreground/80 mt-2 md:text-xl text-base max-w-2xl">
+          I have coded this url shortner using nextjs 14, shadcn/ui & mongodb
+          client mongoose with detailed analytics of links being clicked!
+        </p>
+        <div className="flex items-center sm:justify-center flex-wrap gap-3 mt-3">
+          <Link
+            href="https://github.com/r2hu1/u-sh"
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "flex items-center gap-1",
+            )}
+          >
+            Star on Github
+            <Star className="h-3.5 w-3.5" />
+          </Link>
+          <IfLoggedInElse
+            ifNot={
+              <Link
+                href="/sign-up"
+                className={buttonVariants({ variant: "default" })}
+              >
+                Get Started
+              </Link>
+            }
+            ifUser={
+              <Link
+                href="/dashboard"
+                className={buttonVariants({ variant: "default" })}
+              >
+                Dashboard
+              </Link>
+            }
+          />
+        </div>
+      </div>
+      <div className="w-fit h-fit mt-20 border-4 sm:mt-32 border-input rounded-lg overflow-hidden shadow-lg">
+        <img src="/d-preview-2.png" alt="Hero Image" />
+      </div>
+    </section>
+  );
 }
